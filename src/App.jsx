8b447124b33101ef6cb33742005a432af6e7ae0c
Kalpit Sharma <80953672+Kalpit1998@users.noreply.github.com>
2022-03-09 14:31:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
         <Route path={"/"} element={<Home />} />
         <Route path={"/parkin"} element={<ParkinForm />} />
         <Route path={"/parkout"} element={<Parkoutform />} />
+        <Route path={"*"} element={<h1>Page Not Found</h1>} />
       </Routes>
     </div>
   );
